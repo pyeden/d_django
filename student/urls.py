99b96575662abdd65fadda0e6ce2119fc79a9f6b
“dengs” <1019217919@qq.com>
2021-05-30 18:52:58 +0800

    使用form实现前端添加页面

--- student/urls.py
+++ student/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from student.views import index
 
+app_name = 'student'
+
 urlpatterns = [
     path('', index, name='index'),
 ]
--- student/views.py
+++ student/views.py
@@ -1,8 +1,29 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from .forms import StudentForm
+from .models import Student
+
+
+# def index(request):
+#     words = 'World'
+#     return render(request, 'student/index.html', locals())
 
 
 def index(request):
-    words = 'World'
+    students = Student.objects.all()
+
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('student:index'))
+        else:
+            raise ValueError(form.errors)
+    else:
+        form = StudentForm()
+
     return render(request, 'student/index.html', locals())
